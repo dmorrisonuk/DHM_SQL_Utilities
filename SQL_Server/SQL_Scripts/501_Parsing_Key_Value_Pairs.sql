2b133040ddf1ec28d78ac6abb772eb6896712f26
dmorrisonuk <122388893+dmorrisonuk@users.noreply.github.com>
2025-05-21 15:59:11 +0100

    Update 501_Parsing_Key_Value_Pairs.sql

--- SQL_Server/SQL_Scripts/501_Parsing_Key_Value_Pairs.sql
+++ SQL_Server/SQL_Scripts/501_Parsing_Key_Value_Pairs.sql
@@ -2,21 +2,24 @@
 /* to be used on versions pre JSON function availability */
 
 
+/* it is recommended to avoid using [ ] as the tag delimiters due to inconsitent handling of [ by PATINDEX */
+
 WITH KeyValuePairs AS (
     SELECT 
         t.OriginalString,
-        SUBSTRING(t.OriginalString, p.Pos + 1, CHARINDEX(']', t.OriginalString, p.Pos) - p.Pos - 1) AS KeyValuePair
+        SUBSTRING(t.OriginalString, ca.StartPos + 1, CHARINDEX(']', t.OriginalString, ca.StartPos) - ca.StartPos - 1) AS KeyValuePair
     FROM YourTable t
     CROSS APPLY (
-        SELECT CHARINDEX('[', t.OriginalString, 1) AS Pos
+        SELECT CHARINDEX('<', t.OriginalString, 1) AS StartPos
         UNION ALL
-        SELECT CHARINDEX('[', t.OriginalString, Pos + 1)
-        FROM YourTable
-        WHERE CHARINDEX('[', t.OriginalString, Pos + 1) > 0
-    ) p
+        SELECT CHARINDEX('<', t.OriginalString, StartPos + 1)
+        FROM YourTable t2
+        CROSS APPLY (SELECT CHARINDEX('<', t2.OriginalString, 1) AS StartPos) AS ca2
+        WHERE CHARINDEX('[', t2.OriginalString, StartPos + 1) > 0
+    ) ca
 )
 SELECT 
     OriginalString,
-    PARSENAME(REPLACE(KeyValuePair, ': ', '.'), 2) AS KeyName,
-    PARSENAME(REPLACE(KeyValuePair, ': ', '.'), 1) AS KeyValue
+    LEFT(KeyValuePair, CHARINDEX(':', KeyValuePair) - 1) AS KeyName,
+    RIGHT(KeyValuePair, LEN(KeyValuePair) - CHARINDEX(':', KeyValuePair) - 1) AS KeyValue
 FROM KeyValuePairs;
